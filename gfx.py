514feee8adbcb6b39506dce19302cc84f0557aca
Andrew Friend <mandrewfriend@gmail.com>
2024-07-10 12:04:48 -0400

    Removed commented code, made window members private

--- gfx.py
+++ gfx.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
-    
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -11,33 +11,33 @@ class Window():
         self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        
+
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
-    
+
     def draw_line(self, line, fill_color="black"):
         line.draw(self.canvas, fill_color)
-    
+
     def wait_for_close(self):
         self.running = True
         while self.running:
             self.redraw()
-    
+
     def close(self):
         self.running = False
 
 class Point():
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
 class Line():
-    
+
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        
+
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from gfx import Window
+from gfx import Window, Line, Point
 
 if __name__ == '__main__':
     win = Window(800, 600)
-    #line_1 = Line(Point(10, 10), Point(20, 20))
-    #line_2 = Line(Point(30, 20), Point(40, 50))
-    #win.draw_line(line_1)
-    #win.draw_line(line_2, "red")
+    line_1 = Line(Point(10, 10), Point(20, 20))
+    line_2 = Line(Point(30, 20), Point(40, 100))
+    win.draw_line(line_1)
+    win.draw_line(line_2, "red")
     win.wait_for_close()
