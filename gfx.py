f6dc00e30261ebe39b89812bcd8fb92295f77156
Andrew Friend <mandrewfriend@gmail.com>
2024-07-10 12:53:59 -0400

    Adde cell class and updated main()

--- gfx.py
+++ gfx.py
@@ -3,41 +3,63 @@ from tkinter import Tk, BOTH, Canvas
 class Window():
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root = Tk()
-        self.__root.title("Maze")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(self.__root, width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
+        self._root = Tk()
+        self._root.title("Maze")
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
+        self._canvas = Canvas(self._root, width=width, height=height)
+        self._canvas.pack(fill=BOTH, expand=1)
+        self._running = False
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
+        self._root.update_idletasks()
+        self._root.update()
     def draw_line(self, line, fill_color="black"):
-        line.draw(self.canvas, fill_color)
-
+        line.draw(self._canvas, fill_color)
+    
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self._running = True
+        while self._running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self._running = False
 
 class Point():
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class Line():
-
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color)
+
+class Cell():
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from gfx import Window, Line, Point
+from gfx import Window, Line, Point, Cell
 
 if __name__ == '__main__':
     win = Window(800, 600)
-    line_1 = Line(Point(10, 10), Point(20, 20))
-    line_2 = Line(Point(30, 20), Point(40, 100))
-    win.draw_line(line_1)
-    win.draw_line(line_2, "red")
+    cell_1 = Cell(win)
+    cell_2 = Cell(win)
+
+    cell_1.draw(100, 100, 20, 20)
+    cell_2.draw(30, 30, 50, 50)
+    
     win.wait_for_close()
